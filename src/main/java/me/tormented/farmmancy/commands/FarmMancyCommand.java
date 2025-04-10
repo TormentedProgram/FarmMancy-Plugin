c8ba587a2fa82b4338163c61e5dc6ee004091b9a
Tormented <69302186+TormentedProgram@users.noreply.github.com>
2025-04-11 01:20:04 +1000

    oops forgot the other 2 helps

--- src/main/java/me/tormented/farmmancy/commands/FarmMancyCommand.java
+++ src/main/java/me/tormented/farmmancy/commands/FarmMancyCommand.java
@@ -9,6 +9,8 @@ public class FarmMancyCommand {
         sender.sendMessage("/farmmancy ability set <player> <slot> <unlocked_ability> - Sets the ability based on unlocked abilities for the specified player");
         sender.sendMessage("/farmmancy unlock <players> <ability> - Unlocks specific ability for the specified players");
         sender.sendMessage("/farmmancy farmmancer <add/remove> <players> <amount> <isBaby> - Sets the specified players as farmmancers with the specified amount and if they are babies");
+        sender.sendMessage("/farmmancy menu spawn - Spawns the admin spawn menu for the player");
+        sender.sendMessage("/farmmancy imbue - Imbues the item in hand with powers to control farmmancy");
     }
 
     public FarmMancyCommand() {
